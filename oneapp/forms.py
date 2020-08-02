55529512745e44fa8bc5df33ce9c7046dd780aad
shashank <theshashank6870@gmail.com>
2020-08-02 14:38:35 +0530

    Made Responsive

--- oneapp/forms.py
+++ oneapp/forms.py
@@ -6,11 +6,11 @@ class UrlForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(UrlForm, self).__init__(*args, **kwargs)
         self.fields['short_url'].widget.attrs = {
-            'placeholder': 'Enter a custom short URL',
+            'placeholder': 'Enter a custom URL',
             'id': 'short'
         }
         self.fields['original_url'].widget.attrs = {
-            'placeholder': 'Enter a long URL to make it short',
+            'placeholder': 'Enter a long URL',
             'id': 'long'
         }
 
